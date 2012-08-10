232187ed05c1332f202affadb23d855fb7176148
shinmera <shinmera@tymoon.eu>
2012-08-10 13:24:49 +0200

    Added unspecific data parsing library.

---
+++ src/data/DObject.java
@@ -0,0 +1,84 @@
+/**********************\
+  file: DObject.java
+  package: data
+  author: Shinmera
+  team: NexT
+  license: -
+\**********************/
+
+package data;
+
+import NexT.util.Toolkit;
+import java.util.HashMap;
+
+public class DObject<Type extends Object> {
+    public static final int TYPE_NULL = 0;
+    public static final int TYPE_BOOLEAN = 1;
+    public static final int TYPE_INTEGER = 2;
+    public static final int TYPE_DOUBLE = 3;
+    public static final int TYPE_STRING = 4;
+    public static final int TYPE_OBJECT = 5;
+    
+    private int type = TYPE_NULL;
+    private Type g;
+    private HashMap<String,DObject> o;
+    
+    public DObject(Type t) throws Exception{
+        if(t==null){
+            type = TYPE_NULL;
+            g=null;
+        }if(t instanceof Integer){
+            type = TYPE_INTEGER;
+            g=t;
+        }else if(t instanceof Double){
+            type = TYPE_DOUBLE;
+            g=t;
+        }else if(t instanceof Boolean){
+            type = TYPE_BOOLEAN;
+            g=t;
+        }else if(t instanceof String){
+            type = TYPE_STRING;
+            g=t;
+        }else if(t instanceof HashMap){
+            type = TYPE_OBJECT;
+            o=(HashMap)t;
+        }else{
+            throw new Exception("Bad class type: '"+t.getClass()+"' is not recognized.");
+        }
+    }
+    
+    public static DObject parse(Object o) throws Exception{
+        DObject obj = null;
+        
+        if(o==null){
+            obj = new DObject(null);
+        }else if(o instanceof HashMap){
+            obj = new DObject<HashMap<String,DObject>>((HashMap<String,DObject>)o);
+        }else{
+            try{
+                obj = new DObject<Integer>((Integer)Integer.parseInt(o+""));
+            }catch(Exception ex){
+                try{
+                    obj = new DObject<Double>((Double)Double.parseDouble(o+""));
+                }catch(Exception ex2){
+                    if(o instanceof String){
+                        if(o.equals("false"))    obj = new DObject<Boolean>(Boolean.FALSE);
+                        else if(o.equals("true"))obj = new DObject<Boolean>(Boolean.TRUE);
+                        else                     obj = new DObject<String>(o+"");
+                    }else{
+                        throw new Exception("Bad class type: '"+o.getClass()+"' is not recognized.");
+                    }
+                }
+            }
+        }
+        return obj;
+    }
+    
+    public int getType(){return type;}
+    public boolean is(int type){return (this.type==type);}
+    public Type    get()        {return (g==null) ?  (o==null) ? null : (Type)o : g;}
+    public DObject get(String s){return (o==null) ? null : o.get(s);}
+    public void set(Type t)             {if(g!=null)g=t;else if(o!=null)o=(HashMap)t;}
+    public void set(String s,DObject d) {if(o!=null)o.put(s,d);}
+    public String toString(){return get()+"";}
+}
---
+++ src/data/DParse.java
@@ -0,0 +1,68 @@
+/**********************\
+  file: DParse.java
+  package: data
+  author: Shinmera
+  team: NexT
+  license: -
+\**********************/
+
+package data;
+
+import NexT.util.Toolkit;
+import java.io.File;
+import java.util.HashMap;
+
+public class DParse {
+    public static DObject<HashMap<String,DObject>> parse(File f){return parse(Toolkit.loadFileToString(f));}
+    public static DObject<HashMap<String,DObject>> parse(String s){
+        try{
+            DObject<HashMap<String,DObject>> obj = new DObject<HashMap<String,DObject>>(new HashMap<String,DObject>());
+            s = s.replaceAll("//(.+)","").replaceAll("(?s)/\\*(.+?)\\*/", "").trim();
+            
+            String[] vals = s.split(";");
+            int height = 0;
+            String inside="",okey="",key="",rest="";
+            for(String line : vals){
+                line = line.trim();
+                if(line.contains(":"))key = line.substring(0,line.indexOf(":")).trim();
+                rest= line.substring(line.indexOf(":")+1).trim();
+
+                if("".equals(okey)){
+                    if(rest.startsWith("{")){
+                        okey=key;
+                        System.out.println("BEGIN "+key);
+                    }else{
+                        obj.set(key,DObject.parse(rest));
+                        System.out.println(">"+key+" = "+obj.get(key).get()+" ("+obj.get(key).getType()+")");
+                    }
+                }
+                if(!"".equals(okey)){
+                    inside+=line+';';
+                    height+=Toolkit.countSubstr(line, "\\{");
+                    height+=Toolkit.countSubstr(line, "\\}");
+                    if(height==0){
+                        obj.set(okey,parse(inside.substring(inside.indexOf("{")+1,inside.lastIndexOf("}"))));
+                        System.out.println("END "+okey);
+                        okey="";
+                        inside="";
+                    }
+                }
+            }
+            return obj;
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    
+    public static String parse(DObject<HashMap<String,DObject>> obj){
+        String s = "";
+        HashMap<String,DObject> map = obj.get();
+        for(String key : map.keySet()){
+            DObject val = map.get(key);
+            if(val.is(DObject.TYPE_OBJECT))s+=key+":{"+parse((DObject<HashMap<String,DObject>>)val)+"};";
+            else                           s+=key+":"+val+";";
+        }
+        return s;
+    }
+}
