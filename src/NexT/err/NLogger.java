472fefb4375e3e4fec5dfe5bcc195a4265812d8f
shinmera <shinmera@tymoon.eu>
2012-12-11 22:56:09 +0100

    Minor.

--- src/NexT/err/NLogger.java
+++ src/NexT/err/NLogger.java
@@ -11,10 +11,13 @@ import java.io.File;
 import java.util.logging.Logger;
 
 public abstract class NLogger{
-    public static Logger get(){
-        Logger lg = Logger.getLogger("+");
+    public static Logger get(String name){
+        Logger lg = Logger.getLogger(name);
         lg.setUseParentHandlers(false);
-        lg.addHandler(new NHandler(new File("err.log")));
+        lg.addHandler(new NHandler(new File(name+".log")));
         return lg;
+    }
+    public static Logger get(){
+        return get("err");
     }
 }
