d4f27c21e8160854e8102a1697a201134d29708b
shinmera <shinmera@tymoon.eu>
2013-03-03 16:21:34 +0100

    Added get function for branch notation searching.

--- src/NexT/data/DParse.java
+++ src/NexT/data/DParse.java
@@ -13,7 +13,26 @@ import java.io.File;
 import java.util.HashMap;
 
 public class DParse {
+    
+    /**
+     * Parse a file into a DObject tree.
+     * 
+     * @param f
+     * @return 
+     * @see DParse#parse(java.lang.String) 
+     */
     public static DObject<HashMap<String,DObject>> parse(File f){return parse(Toolkit.loadFileToString(f));}
+    
+    /**
+     * Parse a string into a DObject tree. Sample tree:
+     * field: value;
+     * object: {
+     *   field: value;
+     * };
+     * 
+     * @param s The string to parse
+     * @return 
+     */
     public static DObject<HashMap<String,DObject>> parse(String s){
         DObject<HashMap<String,DObject>> obj = new DObject<HashMap<String,DObject>>(new HashMap<String,DObject>());
         s = s.replaceAll("//(.+)","").replaceAll("(?s)/\\*(.+?)\\*/", "").trim();
@@ -47,12 +66,18 @@ public class DParse {
         return obj;
     }
     
+    /**
+     * Serialise a DObject tree into a string representation.
+     * @param obj
+     * @param beauty Whether to beautify or minify the resulting string.
+     * @return 
+     */
     public static String parse(DObject<HashMap<String,DObject>> obj, boolean beauty){
         if(beauty) return parse(obj, 0);
         else       return parse(obj);
     }
     
-    public static String parse(DObject<HashMap<String,DObject>> obj, int level){
+    private static String parse(DObject<HashMap<String,DObject>> obj, int level){
         StringBuilder s = new StringBuilder();
         HashMap<String,DObject> map = obj.get();
         String padding = (level<=0) ? "" : Toolkit.repeatString(" ", level*4);
@@ -70,7 +95,7 @@ public class DParse {
         return s.toString();
     }
     
-    public static String parse(DObject<HashMap<String,DObject>> obj){
+    private static String parse(DObject<HashMap<String,DObject>> obj){
         StringBuilder s = new StringBuilder();
         HashMap<String,DObject> map = obj.get();
         for(String key : map.keySet()){
@@ -83,5 +108,24 @@ public class DParse {
             }
         }
         return s.toString();
+    }
+    
+    /**
+     * Get a DObject tree leaf through branch notation. IllegalArgumentExceptions
+     * are thrown if a leaf cannot be found or if the base DObject isn't of
+     * TYPE_OBJECT.
+     * 
+     * @param base The root of the DObject tree to search through.
+     * @param branch The branch notation of the leaf we want to access.
+     * @param sep The leaf separator used in the branch notation.
+     * @return 
+     */
+    public static DObject get(DObject base, String branch, String sep){
+        if(base.getType() != DObject.TYPE_OBJECT)throw new IllegalArgumentException("DObject has to be of TYPE_OBJECT.");
+        String[] leaves = branch.split(sep);
+        leaves[0] = leaves[0].trim();
+        if(!base.contains(leaves[0]))throw new IllegalArgumentException("Dobject does not contain leaf '"+leaves[0]+"'.");
+        if(leaves.length == 1)return base.get(leaves[0]);
+        else                  return DParse.get(base.get(leaves[0]), branch.substring(branch.indexOf(sep)+1), sep);
     }
 }
